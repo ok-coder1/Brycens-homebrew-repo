54a8fcee9c2956e238692fdab1551d56bfbb3afc
Brycen G <brycengranville@outlook.com>
2025-06-10 00:39:01 -0400

    fix: resolve syntax errors and declare explicit dependencies for Linux and macOS
    - Bumps SnapX version to `0.2.1` to fix building in Homebrew environment. Previously, the build system, when installing, would try to elevate to root to install files in directories that it believed were privileged.
    - Add X11-related dependencies explicitly for Linux.
    - Add DBus as a dependency for Linux.
    - Add OpenSSL as a dependency for Linux.
    - Mention xdg-utils not being packaged as a caveat for Linux.
    - Mark LLVM toolchain as a build-time dependency, not a general dependency.
    - Only try to use `dotnet-sdk` Cask on macOS.
    - Add Xcode dependency for macOS.
    - Removed distro detection.

--- Formula/s/snapx.rb
+++ Formula/s/snapx.rb
@@ -1,71 +1,62 @@
-class SnapX < Formula
-  desc "SnapX is a free, open-source, cross-platform tool that lets you capture or record any area of your screen and instantly share it with a single keypress. Upload images, videos, text, and more to multiple supported destinations — all with ease."
+class Snapx < Formula
+  desc "Screenshot tool that handles images, text, and video (fork of ShareX)"
   homepage "https://github.com/BrycensRanch/SnapX"
-  version "0.2.0"
-  url "https://github.com/BrycensRanch/SnapX/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "7a163aee1b39a39dd9149a823364fcdf988e6750b0aeb450f8aa15ef4e656d1c"
-  license "GPL-3.0"
+  version "0.2.1"
+  url "https://github.com/BrycensRanch/SnapX/archive/refs/tags/v#{version}.tar.gz"
+  head "https://github.com/BrycensRanch/SnapX.git", branch: "develop"
+  sha256 "ea8ebee68b8c50964a382e20b92ab5c1e85b04515c0d33cce89bd63a63b6b7ee"
+  # Uncomment to bump the package when still using the same SnapX version. Acts like the release field in snapx.spec
+  #revision 1
+  license "GPL-3.0-or-later"
 
-  depends_on macos: ">= :monterey"
+  depends_on macos: :monterey
+  depends_on "git" => :build
+  depends_on "ffmpeg@7"
+  # NativeAOT support
+  depends_on "llvm" => :build
 
-  def detect_distro
-    on_linux do
-      os_release = if File.exist?("/etc/os-release")
-                    File.read("/etc/os-release")
-                  else
-                    Utils.safe_popen_read("lsb_release", "-si").strip
-                  end
-    
-      if os_release.include?("UBUNTU") || os_release.include?("ubuntu") || os_release.include?("Ubuntu")
-        :ubuntu
-      elsif os_release.include?("FEDORA") || os_release.include?("fedora") || os_release.include?("Fedora")
-        :fedora
-      else
-        :unknown
-      end
-    end
+  on_macos do
+    # The .NET SDK Cask has a higher feature band.
+    # Also supports older versions of macOS.
+    depends_on "dotnet-sdk" => :build
+    # Screenshotting on macOS is done via a Rust compat layer. We must compile it.
+    depends_on "rust" => :build
+    # NativeAOT support
+    depends_on xcode: :build
+  end
+  on_linux do
+    # Fallback to dotnet formula that is cross-platform.
+    depends_on "dotnet" => :build
+    depends_on "libx11"
+    depends_on "libsm"
+    depends_on "libxrandr"
+    depends_on "libxcb"
+    depends_on "dbus"
+    depends_on "openssl@3"
   end
 
   def install
-    if OS.mac?
-      ohai "OS: macOS"
-      depends_on "ffmpeg@7"
-      depends_on "rust"
-    end
-    if OS.linux?
-      case detect_distro
-      when :ubuntu
-        ohai "OS: Ubuntu"
-        depends_on "dotnet-sdk"
-        depends_on "ffmpeg@7"
-        depends_on "llvm"
-        depends_on "zlib"
-        depends_on "libsm"
-      when :fedora
-        ohai "OS: Fedora"
-        depends_on "git"
-        depends_on "dotnet-sdk"
-        depends_on "ffmpeg@7"
-      else
-        opoo "Unsupported linux distribution (if using Arch Linux please use AUR package). Continuing..."
-    end
-    ENV["SKIP_MACOS_VERSION_CHECK"] = 1
-    system "./build.sh"
+    ENV["SKIP_MACOS_VERSION_CHECK"] = "1"
+    ENV["ELEVATION_NOT_NEEDED"] = "1"
+    system "./build.sh install --prefix / --dest-dir '#{prefix}'"
   end
-end
 
-def caveats
-  <<~EOS
+  test do
+    system bin/"snapx", "--version"
+  end
+
+  def caveats
+    <<~EOS
     On Ubuntu, you need to run
-      sudo apt install -y libvlc-dev zlib1g-dev
+      sudo apt install -y libvlc-dev zlib1g-dev xdg-utils
     On Fedora, you need to run
-    sudo dnf in -y vlc-devel
-
-    Additionally, it seems SnapX hasn't been able to create the configuration file(s) it expects.
+      sudo dnf in -y vlc-devel xdg-utils
+    Additionally, SnapX hasn't been able to create the configuration file(s) it expects.
     You should place it in the configuration directory that it expects.
     On Linux, it's
       ~/.config/SnapX
     On macOS, it's
       ~/Library/Application Support/SnapX
-  EOS
+    EOS
+  end
 end
